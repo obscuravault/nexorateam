11fe4a9f4215f440a8edcf3e7c90b7bd7fb23d49
obscuravault <mail.obscuravault@gmail.com>
2025-10-29 20:47:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
     API_KEY: 'AIzaSyDzdiDMtiY250DS-lDuZeIZOocw9oqMlhM', // Replace with your API key
-    SPREADSHEET_ID: '1XFR_1XAV5rHr6Sbmc9wqy363UjzZqb_7i4fPBRQcxBE/' // Replace with your spreadsheet ID
+    SPREADSHEET_ID: '1XFR_1XAV5rHr6Sbmc9wqy363UjzZqb_7i4fPBRQcxBE' // Replace with your spreadsheet ID
 };
