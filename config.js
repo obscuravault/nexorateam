a9b17f002482d9deca2c116d15aad8c3aed75e85
obscuravault <mail.obscuravault@gmail.com>
2025-10-29 12:00:11 +0530

    Create  config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+    API_KEY: 'YOUR_GOOGLE_SHEETS_API_KEY_HERE', // Replace with your API key
+    SPREADSHEET_ID: 'YOUR_SPREADSHEET_ID_HERE' // Replace with your spreadsheet ID
+};
